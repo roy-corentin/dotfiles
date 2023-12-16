a5f02feb4e77ab7d613687ea70cc549ad1b3cb54
roy-corentin <corentin.roy@epitech.eu>
2023-12-16 18:39:58 +0100

    [UP] script make change cooler

--- wofi/wallpaper.sh
+++ wofi/wallpaper.sh
@@ -6,8 +6,6 @@ wallpapers="$home/Pictures/wallpapers"
 image=$(ls "$wallpapers" | wofi --dmenu --prompt 'Select wallpaper')
 wallpaper="$wallpapers/$image"
 
-echo "$image" >> ~/wallpaper.log
-
 if [[ -d $wallpaper ]]; then
   echo "$wallpaper is a directory"
   wallpaper_temp="$image"
@@ -20,15 +18,10 @@ else
   exit 1
 fi
 
-echo "$wallpaper" >> ~/wallpaper.log
+swww img "$wallpaper" --transition-type grow --transition-fps 60 --transition-duration 0.5 --transition-bezier 0.65,0,0.35,1 --transition-step 1
+
+sleep 1.25
 
 wal -i "$wallpaper"
 
-echo $? >> ~/wallpaper.log
-
 ~/Application/ChromiumPywal/generate-theme.sh
-
-cat ~/.cache/wal/sequences
-
-swww img "$wallpaper" --transition-type grow --transition-fps 60 --transition-duration 0.5 --transition-bezier 0.65,0,0.35,1 --transition-step 1
-
