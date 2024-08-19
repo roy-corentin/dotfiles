e1aab24d3922740ed7d72e7865e867e9fcca5299
roy-corentin <corentin.roy@epitech.eu>
2024-08-19 22:33:34 +0200

    [UP] script to show keyding without "bind =" + like table thx ','

--- .config/rofi/keybinding.sh
+++ .config/rofi/keybinding.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rg "bind =" ~/.config/hypr/hyprland.conf | rofi -dmenu
+rg "^bind =" ~/.config/hypr/hyprland.conf | sed 's/bind = //g' | awk -F, '{printf "%-15s %-10s %-10s %s\n", $1, $2, $3, $4}' | rofi -dmenu -i -p "Keybinds"
