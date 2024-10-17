6dbeecb9324685da90a9547d63de33e2256c4969
roy-corentin <corentin.roy@epitech.eu>
2024-10-17 12:21:43 +0200

    [UP] xdg-desktop-portal script

--- .config/hypr/scripts/desktop-portals.sh
+++ .config/hypr/scripts/desktop-portals.sh
@@ -3,9 +3,8 @@ sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gnome
 killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-kde
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
-/usr/lib/xdg-desktop-portal-kde &
-# /usr/lib/xdg-desktop-portal-wlr &
 sleep 2
 /usr/lib/xdg-desktop-portal &
