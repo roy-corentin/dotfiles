afd49b63cdf65c4c1a4807216e0acf45b9591c0a
roy-corentin <corentin.roy@epitech.eu>
2024-11-28 20:43:21 +0100

    [UP] call my custom emacs cmd to change theme

--- .config/rofi/theme.sh
+++ .config/rofi/theme.sh
@@ -8,7 +8,7 @@ fi
 if theme=$(wal --theme | awk -F ' - ' '/^[[:space:]]*-/ { print $NF }' | rofi -dmenu -mesg 'Select theme' | sed "s/ (last used)//"); then
     echo "$theme"
     wal --cols16 --theme "$theme"
-    emacsclient -e "(load-theme '$theme t)"
+    emacsclient -e "(cr/switch-theme '$theme)"
     # swaync-client -rs
 else
     echo "$theme"
