8d7aa86f7f67ac3beb29a9eb7bdb76f1c1b54e38
roy-corentin <corentin.roy@epitech.eu>
2025-02-03 11:25:53 +0100

    [UP] justify name compact mode
    Previously "compactmode" was instead an "extandmode"
    So I inverse different gaps to justify the name "compactmode"

--- .config/hypr/scripts/compactmode.sh
+++ .config/hypr/scripts/compactmode.sh
@@ -2,8 +2,8 @@
 
 COMPACTMODE=$(hyprctl getoption general:gaps_out | awk 'NR==1{print $3}')
 
-if [ "$COMPACTMODE" = 4 ] ; then
-  hyprctl --batch "keyword general:gaps_out 20;keyword general:gaps_in 12"
+if [ "$COMPACTMODE" != 4 ] ; then
+  hyprctl --batch "keyword general:gaps_in 2;keyword general:gaps_out 4"
   exit
 fi
 hyprctl reload
