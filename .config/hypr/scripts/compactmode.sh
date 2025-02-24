dde689c02bb2c595838e89aa9cf98dc9f52ee380
roy-corentin <corentin.roy@epitech.eu>
2025-02-24 10:15:18 +0100

    [UP] make compact mode more compact + use var

--- .config/hypr/scripts/compactmode.sh
+++ .config/hypr/scripts/compactmode.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env sh
 
 COMPACTMODE=$(hyprctl getoption general:gaps_out | awk 'NR==1{print $3}')
+GAPS_IN=0
+GAPS_OUT=0
+BORDER_SIZE=0
+ROUNDIN=0
 
-if [ "$COMPACTMODE" != 4 ] ; then
-  hyprctl --batch "keyword general:gaps_in 2;keyword general:gaps_out 4"
+if [ "$COMPACTMODE" != $GAPS_OUT ] ; then
+    hyprctl --batch "keyword general:gaps_in $GAPS_IN;\
+                     keyword general:gaps_out $GAPS_OUT;\
+                     keyword general:border_size $BORDER_SIZE;\
+                     keyword decoration:rounding $ROUNDING"
   exit
 fi
 hyprctl reload
