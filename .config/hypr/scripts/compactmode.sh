f0e416b5eea0513b15823bf568f61398ee38ccf1
roy-corentin <corentin.roy@epitech.eu>
2025-02-27 21:25:47 +0100

    [ADD] border size to 1 in compact mode

--- .config/hypr/scripts/compactmode.sh
+++ .config/hypr/scripts/compactmode.sh
@@ -3,7 +3,7 @@
 COMPACTMODE=$(hyprctl getoption general:gaps_out | awk 'NR==1{print $3}')
 GAPS_IN=0
 GAPS_OUT=0
-BORDER_SIZE=0
+BORDER_SIZE=1
 ROUNDING=0
 
 if [ "$COMPACTMODE" != $GAPS_OUT ] ; then
