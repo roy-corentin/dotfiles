534177ee04843e1d6b91e38e3409f9763c28d72c
roy-corentin <corentin.roy@epitech.eu>
2025-03-13 20:00:58 +0100

    [ADD] new 'gamemode' mode

---
+++ .config/hypr/scripts/gamemode.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    systemctl --user stop kanata.service
+    hyprctl keyword monitor DP-1, 1920x1080@60, 2000, 1
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:shadow:enabled 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+    exit
+fi
+systemctl --user restart kanata.service
+hyprctl reload
