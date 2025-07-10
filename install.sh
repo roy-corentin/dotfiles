e1eaa48d952c8eb6f246eff0e8a3ebacc588ad4c
roy-corentin <corentin.roy@epitech.eu>
2025-07-10 16:09:39 +0200

    [UP] improve install script

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ stow .
 
 echo "Done."
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Give people a chance to retry running the installation
+trap 'echo "Omarchy installation failed! You can retry by running: source ~/.local/share/omarchy/install.sh"' ERR
+
 echo "Install Lib"
 yay -S cmake libgccjit
 
@@ -21,7 +27,8 @@ yay -S hyprland waybar rofi-wayland dunst dolphin xdg-desktop-portal-hyprland qt
     zsh zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions \
     postgresql-libs platformio libvips compiledb \
     emacs-lsp-booster zls \
-    v4l2loopback-dkms intel-ipu6-dkms intel-npu-driver
+    v4l2loopback-dkms intel-ipu6-dkms intel-npu-driver \
+    localsend-bin impala
 
 gum confirm 'Would you like to install kanata?' && ./install-kanata.sh
 gum confirm "Would you like to install fonts" && echo "Install fonts" && yay -S ttc-iosevka ttc-iosevka-etoile ttc-iosevka-term ttf-iosevka-nerd
