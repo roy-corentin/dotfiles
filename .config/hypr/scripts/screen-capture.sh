ce3a7e21243124856c536ebd0c874158061b9d38
roy-corentin <corentin.roy@epitech.eu>
2025-10-09 22:48:27 +0200

    [UP] notificaton on multi line

--- .config/hypr/scripts/screen-capture.sh
+++ .config/hypr/scripts/screen-capture.sh
@@ -27,7 +27,7 @@ stop_screenrecording() {
   pkill -x wl-screenrec
   pkill -x wf-recorder
 
-  notify-send "Screen recording saved to $OUTPUT_DIR" -t 2000
+  notify-send "Screen recording saved" "to $OUTPUT_DIR" -t 5000
 
   sleep 0.2 # ensures the process is actually dead before we check
   toggle_screenrecording_indicator
