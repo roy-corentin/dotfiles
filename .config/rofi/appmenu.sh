eb1c33c03325a467aadd4c29b74c665b060ccf2c
roy-corentin <corentin.roy@epitech.eu>
2025-10-14 17:38:24 +0200

    [UP] use uwsm-app in more place

--- .config/rofi/appmenu.sh
+++ .config/rofi/appmenu.sh
@@ -5,4 +5,4 @@ if [[ $(pidof rofi) ]]; then
     exit 1
 fi
 
-rofi -show combi -combi-modes "window,drun,run" -modes "combi,window,filebrowser" -show-icons -p " " -theme '~/.config/rofi/appmenu.rasi'
+rofi -show combi -combi-modes "window,drun,run" -modes "combi,window,filebrowser" -run-command "uwsm-app -- {cmd}" -show-icons -p " " -theme '~/.config/rofi/appmenu.rasi'
