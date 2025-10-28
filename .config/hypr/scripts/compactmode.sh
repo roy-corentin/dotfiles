0165bbdbf048020b50a67ff4e5b58afd5f9e9ba3
roy-corentin <corentin.roy@epitech.eu>
2025-10-28 15:00:02 +0100

    [UP] rename vars in compact mode

--- .config/hypr/scripts/compactmode.sh
+++ .config/hypr/scripts/compactmode.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
-COMPACTMODE=$(hyprctl getoption general:gaps_out | awk 'NR==1{print $3}')
-GAPS_IN=0
-GAPS_OUT=0
-BORDER_SIZE=1
-ROUNDING=0
+GAPSOUT=$(hyprctl getoption general:gaps_out | awk 'NR==1{print $3}')
+COMPACTED_GAPS_IN=0
+COMPACTED_GAPS_OUT=0
+COMPACTED_BORDER_SIZE=1
+COMPACTED_ROUNDING=0
 
-if [ "$COMPACTMODE" != $GAPS_OUT ] ; then
-    hyprctl --batch "keyword general:gaps_in $GAPS_IN;\
-                     keyword general:gaps_out $GAPS_OUT;\
-                     keyword general:border_size $BORDER_SIZE;\
-                     keyword decoration:rounding $ROUNDING;\
-                     keyword decoration:rounding_power $ROUNDING;"
-  exit
+if [ "$GAPSOUT" != $COMPACTED_GAPS_OUT ] ; then
+    hyprctl --batch "keyword general:gaps_in $COMPACTED_GAPS_IN;\
+                     keyword general:gaps_out $COMPACTED_GAPS_OUT;\
+                     keyword general:border_size $COMPACTED_BORDER_SIZE;\
+                     keyword decoration:rounding $COMPACTED_ROUNDING;\
+                     keyword decoration:rounding_power $COMPACTED_ROUNDING;"
+else
+    hyprctl reload
 fi
-hyprctl reload
