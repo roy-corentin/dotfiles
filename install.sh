4cbe32d690181dc4a0b2a8cf7d9b2401c82fe834
roy-corentin <corentin.roy@epitech.eu>
2025-11-27 17:22:00 +0100

    [ADD] package in install script

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ echo "Install packages"
 yay -S hyprland waybar rofi-wayland dunst dolphin xdg-desktop-portal-hyprland qt5-wayland qt6-wayland wtype \
     hyprpaper hyprpicker hyprlock hypridle hyprshade hyprcursor hyprshot waypaper cliphist grim slurp tesseract swappy satty gowall cava \
     brightnessctl mako wget unzip rsync figlet git network-manager-applet python-pywal16 power-profile-daemon \
-    pulsemixer ripgrep eza bat btop fd tldr calcurse tree-sitter imagemagick pavucontrol wiremix \
+    pulsemixer ripgrep eza bat btop fd tldr calcurse tree-sitter imagemagick pavucontrol wiremix bluetui \
     zsh zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions \
     postgresql-libs platformio libvips compiledb \
     emacs-lsp-booster zls \
