7ae715a8018f52076bd913b95b5033491c329564
roy-corentin <corentin.roy@epitech.eu>
2026-06-24 17:01:32 +0200

    del(hyprshade): not use anymore

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ yay -S extra/texlive-latex extra/texlive-latexextra extra/texlive-plaingeneric t
 
 echo "Install packages"
 yay -S hyprland waybar rofi-wayland dunst dolphin xdg-desktop-portal-hyprland qt5-wayland qt6-wayland wtype \
-    hyprpaper hyprpicker hyprlock hypridle hyprshade hyprcursor hyprshot waypaper cliphist grim slurp tesseract swappy satty gowall cava \
+    hyprpaper hyprpicker hyprlock hypridle hyprcursor hyprshot waypaper cliphist grim slurp tesseract swappy satty gowall cava \
     brightnessctl mako wget unzip rsync figlet git network-manager-applet python-pywal16 power-profile-daemon \
     pulsemixer ripgrep eza bat btop fd tldr delta dust calcurse tree-sitter imagemagick pavucontrol wiremix bluetui \
     zsh zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions \
