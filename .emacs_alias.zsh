78491de2f47c563da42a0cc90f4f9d46721bf000
roy-corentin <corentin.roy@epitech.eu>
2026-06-24 17:02:34 +0200

    fix: pass arguments to emacsclient in et alias

--- .emacs_alias.zsh
+++ .emacs_alias.zsh
@@ -4,4 +4,4 @@ e()     { pgrep emacs && emacsclient -n "$@" || emacs -nw "$@" }
 ediff() { emacs -nw --eval "(ediff-files \"$1\" \"$2\")"; }
 eman()  { emacs -nw --eval "(switch-to-buffer (man \"$1\"))"; }
 ekill() { emacsclient --eval '(kill-emacs)'; }
-et()    { emacsclient -t }
+et()    { emacsclient -t "$@" }
